037c52e228c75b67f7396059cb65a9bce067aa63
Antonio Carrascosa Jiménez <Antonio.Carrascosa2@alu.uclm.es>
2024-12-11 10:31:03 +0100

    Añadidos atributos que definen el equipo del agente y las reglas.

--- Codigo/src/Agente.java
+++ Codigo/src/Agente.java
@@ -4,6 +4,7 @@ import java.util.concurrent.*;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Random;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -18,7 +19,6 @@ import XML.MessageGenerator;
 import com.sun.tools.javac.Main;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import java.io.StringWriter;
 
 import static java.lang.System.exit;
 
@@ -26,9 +26,12 @@ public class Agente {
 
     Thread sendBroadcastThread; // Hilo del agente para enviar los mensajes de Broadcast
     Thread listenerThread; // Hilo del agente que escucha mensajes directos recibidos
-    Thread listenerForBroadcast; // Hilo del agente que escucha mensajes de broadcast de otros agentes y registra su IP
+    Thread listenerForBroadcast; // Hilo del agente que escucha mensajes de broadcast de otros agentes y registra
+                                 // su IP
+
     private InetAddress monitorAddress; // Dirección IP del monitor al que mandará cierta información el agente
     private Integer monitorPort; // Puerto por el que se comunicarán los agentes con el monitor
+
     // private DatagramSocket sendSocket;
     private ServerSocket listenSocket; // Socket para recibir mensajes directos
     private Integer listeningPort; // Puerto de listenSocket
@@ -36,11 +39,17 @@ public class Agente {
     private InetAddress ip; // Ip del agente
     private Integer netMask; // Máscara de la red en la que se encuentra el agente
     private InetAddress subNet; // Submáscara de la red en la que se encuentra el agente
-    private List<AgenteConocido> agentesDescubiertos; // Lista de todos los agentes registrados de un agente con los que se ha comunicado
+
+    private List<AgenteConocido> agentesDescubiertos; // Lista de todos los agentes registrados de un agente con los que
+                                                      // se ha comunicado
+
     private String equipoDelAgente; // Equipo al que pertenece el agente antes de empezar los duelos
+    private String equipoFachada; // Equipo que el agente envía originalmente que puede o no ser el mismo que el
+                                  // equipo del agente
+    private double probabilidadMandarFachada; // Probabilidad de que el agente mande su equipo fachada
 
-
-    // Método constructor del agente donde se hará la búsqueda de nido y se asignará un puerto y IP para comunicarse
+    // Método constructor del agente donde se hará la búsqueda de nido y se asignará
+    // un puerto y IP para comunicarse
     public Agente(InetAddress monitorAddress, int monitorPort) throws IOException {
         this.monitorAddress = monitorAddress;
         this.monitorPort = monitorPort;
@@ -73,13 +82,42 @@ public class Agente {
         System.out.println("Máscara de red: " + this.netMask);
         System.out.println("Puerto de escucha: " + this.listeningPort);
         System.out.println("Numero de IPs disponibles: " + this.ipList.size());
+
+        // Escoger equipo del agente
+        Random random = new Random();
+
+        List<String> opciones = new ArrayList<String>();
+        opciones.add("Piedra");
+        opciones.add("Papel");
+        opciones.add("Tijera");
+        opciones.add("Lagarto");
+        opciones.add("Spock");
+        opciones.add("Bebe_que_tose");
+        opciones.add("Papa_Noel");
+        opciones.add("Mesa_de_IKEA");
+        opciones.add("Bomba_de_Hidrógeno");
+
+        // se generan 2 opciones al azar
+        int i1 = random.nextInt(opciones.size());
+        int i2 = random.nextInt(opciones.size());
+
+        this.equipoDelAgente = opciones.get(i1);
+        this.equipoFachada = opciones.get(i2);
+
+        // Probabilidad de mandar fachada en lugar del equipo real
+        this.probabilidadMandarFachada = 0.3 + (0.6 - 0.3) * random.nextDouble();
     }
 
     // Función de replicación del agente (se usará una vez gane los duelos)
-    // Permite que un agente se duplique tras ganar los duelos contra otro. Provocando que el agente sea del mismo equipo
+    // Permite que un agente se duplique tras ganar los duelos contra otro.
+    // Provocando que el agente sea del mismo equipo
     public void replicacionDelAgente() throws IOException {
-        //ProcessBuilder processBuilder = new ProcessBuilder("java","-cp","C:/Users/Usuario/IdeaProjects/Proyecto SMA cuarto año/Codigo/src", "Agente");
-        //ProcessBuilder processBuilder = new ProcessBuilder("java","-cp","C:/Users/Usuario/IdeaProjects/Proyecto SMA cuarto año/Codigo/out/production/cosoSMA", "Agente");
+        // ProcessBuilder processBuilder = new
+        // ProcessBuilder("java","-cp","C:/Users/Usuario/IdeaProjects/Proyecto SMA
+        // cuarto año/Codigo/src", "Agente");
+        // ProcessBuilder processBuilder = new
+        // ProcessBuilder("java","-cp","C:/Users/Usuario/IdeaProjects/Proyecto SMA
+        // cuarto año/Codigo/out/production/cosoSMA", "Agente");
         ProcessBuilder processBuilder = new ProcessBuilder("java", "Agente");
         processBuilder.start();
     }
@@ -89,38 +127,40 @@ public class Agente {
         System.out.println("Me muero a");
         String msg = null;
         try {
-            msg = MessageGenerator.createHeMuertoMessage(this.listeningPort,"Com1","Msg1","Agente_01",this.ip.toString(),"Monitor","192.168.1.168");
+            msg = MessageGenerator.createHeMuertoMessage(this.listeningPort, "Com1", "Msg1", "Agente_01",
+                    this.ip.toString(), "Monitor", "192.168.1.168");
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (TransformerException e) {
             e.printStackTrace();
         }
         this.sendToMonitor(msg);
-        if(listenerThread != null){
+        if (listenerThread != null) {
             listenerThread.interrupt();
             listenerThread.join(2000);
         }
-        if(listenerForBroadcast != null) {
+        if (listenerForBroadcast != null) {
             listenerForBroadcast.interrupt();
             listenerForBroadcast.join(2000);
         }
-        if(sendBroadcastThread != null){
+        if (sendBroadcastThread != null) {
             sendBroadcastThread.interrupt();
             sendBroadcastThread.join(2000);
         }
         System.exit(0);
     }
 
-    // Función de parada del agente (se usará una vez acaben los duelos y el agente acabe con vida con su equipo)
+    // Función de parada del agente (se usará una vez acaben los duelos y el agente
+    // acabe con vida con su equipo)
     private synchronized void paradaDelAgente() throws InterruptedException {
         System.out.println("Tomaaaaaaaa");
     }
 
-    private void continuarAgente(){
+    private void continuarAgente() {
         System.out.println("Allevoy");
     }
 
-    //El agente obtendra la IP a la que asociarse
+    // El agente obtendra la IP a la que asociarse
     public void getIp() {
         try {
             // Obtener las interfaces de red
@@ -147,7 +187,7 @@ public class Agente {
         }
     }
 
-    //El agente obtendrá una vez tenida su IP, la máscara de red asociada
+    // El agente obtendrá una vez tenida su IP, la máscara de red asociada
     public void getMascaraDeRed() {
         try {
             // Obtener la interfaz de red asociada a la dirección IP proporcionada
@@ -169,7 +209,7 @@ public class Agente {
         System.out.println("No se pudo encontrar la máscara de red para la dirección IP: " + this.ip);
     }
 
-    //En esta función obtendrá el agente la subred en la que estará situado
+    // En esta función obtendrá el agente la subred en la que estará situado
     public void getSubNet() {
         try {
             InetAddress ip = this.ip;
@@ -215,7 +255,7 @@ public class Agente {
         }
     }
 
-    //Pasar el array de bytes de 4 elementos a un entero de 32 bits
+    // Pasar el array de bytes de 4 elementos a un entero de 32 bits
     private int bytesToInt(byte[] bytes) {
         int result = 0;
         for (byte b : bytes) {
@@ -234,7 +274,7 @@ public class Agente {
         };
     }
 
-    //El agente obtiene toda la lista de ips disponibles
+    // El agente obtiene toda la lista de ips disponibles
     public List<InetAddress> getAvailableIPs() {
         List<InetAddress> availableIPs = new ArrayList<>();
 
@@ -265,8 +305,9 @@ public class Agente {
     //////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////
 
-    //Enviamos un mensaje por broadcast a cada una de las ips por cada uno de los puertos disponibles
-    public void sendBroadcast(String message, int waitTime) throws InterruptedException{
+    // Enviamos un mensaje por broadcast a cada una de las ips por cada uno de los
+    // puertos disponibles
+    public void sendBroadcast(String message, int waitTime) throws InterruptedException {
         sendBroadcastThread = new Thread(() -> {
             while (!sendBroadcastThread.isInterrupted()) {
                 for (InetAddress ip : this.ipList) {
@@ -298,13 +339,14 @@ public class Agente {
     public void listenForBroadcast() {
         listenerForBroadcast = new Thread(() -> {
             try (DatagramSocket discoverySocket = new DatagramSocket(this.listeningPort)) {
-                //System.out.println("Agente escuchando mensajes de broadcast en el puerto UDP " + this.listeningPort);
+                // System.out.println("Agente escuchando mensajes de broadcast en el puerto UDP
+                // " + this.listeningPort);
                 byte[] buffer = new byte[1024];
 
                 while (!listenerForBroadcast.isInterrupted()) {
 
                     DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                    discoverySocket.receive(packet);  // Recibe el mensaje de broadcast
+                    discoverySocket.receive(packet); // Recibe el mensaje de broadcast
 
                     String message = new String(packet.getData(), 0, packet.getLength());
                     InetAddress senderAddress = packet.getAddress();
@@ -316,7 +358,8 @@ public class Agente {
                         continue; // Saltar a la siguiente iteración del bucle
                     }
 
-                    System.out.println("Mensaje de broadcast recibido de " + senderAddress + ": [Port: " + senderPort + "]: " + message);
+                    System.out.println("Mensaje de broadcast recibido de " + senderAddress + ": [Port: " + senderPort
+                            + "]: " + message);
 
                     AgenteConocido nuevoAgente = new AgenteConocido(senderAddress, senderPort);
                     if (!agentesDescubiertos.contains(nuevoAgente)) {
@@ -352,8 +395,9 @@ public class Agente {
                 while (!listenerThread.isInterrupted()) {
 
                     try (Socket clientSocket = listenSocket.accept();
-                         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                            BufferedReader in = new BufferedReader(
+                                    new InputStreamReader(clientSocket.getInputStream()));
+                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
                         InetAddress senderAddress = clientSocket.getInetAddress();
                         int senderPort = clientSocket.getPort();
@@ -376,16 +420,12 @@ public class Agente {
                 }
             } catch (BindException y) {
                 System.out.println("Escucho mensajes");
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         });
         listenerThread.start();
     }
-
-
-
 
     // Enviar respuesta directa a otro agente
     public void sendDirectMessage(String message, InetAddress address, int puertoDestino) {
@@ -398,8 +438,157 @@ public class Agente {
     //////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////
 
-    //Función específica de los agentes (Duelo de Piedra, Papel, Tijera, Lagarto, Spock, Bebé que tose, Papá Noel, Mesa de IKEA y Bomba de Hidrógeno)
-    public void FuncionDeAgente(){
+    /*
+     * Método para resolver un duelo entre dos agentes
+     * 
+     * @param equipoRival: Equipo del agente rival
+     * 
+     * @return: 0 si el agente (yo) pierde, 1 si el agente (yo) gana, 2 si hay
+     * empate
+     */
+    public int resolverDuelo(String equipoRival) {
+
+        // Si el agente rival es del mismo equipo que el agente (yo)
+        if (this.equipoDelAgente == equipoRival) {
+            return 2;
+        }
+
+        // En caso de que sean equipos distintos
+
+        // Si el agente es Piedra
+        if (this.equipoDelAgente == "Piedra" && equipoRival == "Bebe_que_tose") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Piedra" && equipoRival == "Bomba_de_Hidrogeno") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Piedra" && equipoRival == "Lagarto") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Piedra" && equipoRival == "Tijera") {
+            return 1;
+        }
+
+        // Si el agente es Papel
+        if (this.equipoDelAgente == "Papel" && equipoRival == "Piedra") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Papel" && equipoRival == "Spock") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Papel" && equipoRival == "Bomba_de_Hidrogeno") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Papel" && equipoRival == "Santa_Claus") {
+            return 1;
+        }
+
+        // Si el agente es Tijera
+        if (this.equipoDelAgente == "Tijera" && equipoRival == "Papel") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Tijera" && equipoRival == "Lagarto") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Tijera" && equipoRival == "Mesa_de_IKEA") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Tijera" && equipoRival == "Santa_Claus") {
+            return 1;
+        }
+
+        // Si el agente es Lagarto
+        if (this.equipoDelAgente == "Lagarto" && equipoRival == "Papel") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Lagarto" && equipoRival == "Spock") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Lagarto" && equipoRival == "Santa_Claus") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Lagarto" && equipoRival == "Bebe_que_tose") {
+            return 1;
+        }
+
+        // Si el agente es Spock
+        if (this.equipoDelAgente == "Spock" && equipoRival == "Piedra") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Spock" && equipoRival == "Tijera") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Spock" && equipoRival == "Mesa_de_IKEA") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Spock" && equipoRival == "Bomba_de_Hidrogeno") {
+            return 1;
+        }
+
+        // Si el agente es Bebe_que_tose
+        if (this.equipoDelAgente == "Bebe_que_tose" && equipoRival == "Papel") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Bebe_que_tose" && equipoRival == "Spock") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Bebe_que_tose" && equipoRival == "Santa_Claus") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Bebe_que_tose" && equipoRival == "Tijera") {
+            return 1;
+        }
+
+        // Si el agente es Bomba_de_Hidrogeno
+        if (this.equipoDelAgente == "Bomba_de_Hidrogeno" && equipoRival == "Lagarto") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Bomba_de_Hidrogeno" && equipoRival == "Beba_que_tose") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Bomba_de_Hidrogeno" && equipoRival == "Mesa_de_IKEA") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Bomba_de_Hidrogeno" && equipoRival == "Tijera") {
+            return 1;
+        }
+
+        // Si el agente es Santa_Claus
+        if (this.equipoDelAgente == "Santa_Claus" && equipoRival == "Piedra") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Santa_Claus" && equipoRival == "Mesa_de_IKEA") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Santa_Claus" && equipoRival == "Bomba_de_Hidrogeno") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Santa_Claus" && equipoRival == "Spock") {
+            return 1;
+        }
+
+        // Si el agente es Mesa_de_IKEA
+        if (this.equipoDelAgente == "Mesa_de_IKEA" && equipoRival == "Piedra") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Mesa_de_IKEA" && equipoRival == "Lagarto") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Mesa_de_IKEA" && equipoRival == "Papel") {
+            return 1;
+        }
+        if (this.equipoDelAgente == "Mesa_de_IKEA" && equipoRival == "Bebe_que_tose") {
+            return 1;
+        }
+
+        // si no se cumple ninguna de las anteriores, es que este agente (yo) ha perdido
+        return 0;
+
+    }
+
+    // Función específica de los agentes (Duelo de Piedra, Papel, Tijera, Lagarto,
+    // Spock, Bebé que tose, Papá Noel, Mesa de IKEA y Bomba de Hidrógeno)
+    public void FuncionDeAgente() {
         System.out.println("UGHHH!");
     }
 
@@ -408,7 +597,6 @@ public class Agente {
     ///////////////////////////// Hasta aqúi la Funcion del Agente
     //////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////
-
 
     // Enviar copia del mensaje al monitor
     public void sendToMonitor(String message) {
@@ -434,9 +622,13 @@ public class Agente {
 
         Agente agente = new Agente(monitorAddress, monitorPort);
 
+        System.err.println("Hola soy un pelotudo de equipo: " + agente.equipoDelAgente);
+        System.err.println("Hola soy un pelotudo de equipo fachada: " + agente.equipoFachada);
+
         String msg = null;
         try {
-            msg = MessageGenerator.createHeNacidoMessage(agente.listeningPort,"Com1","Msg1","Agente_01",agente.ip.toString(),"Monitor","192.168.1.180");
+            msg = MessageGenerator.createHeNacidoMessage(agente.listeningPort, "Com1", "Msg1", "Agente_01",
+                    agente.ip.toString(), "Monitor", "192.168.1.180");
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (TransformerException e) {
